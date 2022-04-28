ed350ca4e5289a67ec77ee980ecba6dc96411bb3
Kirill Marchuk <kirill.marchuk@bwt.ee>
2022-04-28 13:17:31 +0300

    initial commit

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Application {
+    public static void main(String[] args) {
+        var model = new Model();
+        System.out.println(model.getIntField());
+    }
+}
---
+++ src/main/java/org/example/Model.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class Model {
+    private Integer intField = 10;
+}
